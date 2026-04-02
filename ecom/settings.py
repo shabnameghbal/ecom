eb5302933297a2f08adedd1d3f71f6bc7e847142
shabnameghbal <eghbalshabnam@gmail.com>
2026-04-02 12:29:07 +0530

    added data with superuser

--- ecom/settings.py
+++ ecom/settings.py
@@ -88,9 +88,19 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+
+
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
